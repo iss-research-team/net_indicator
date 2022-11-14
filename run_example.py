e1a807bc45e51003c629252bd6f32e514d28e78c
HadesMiss <1226869963@qq.com>
2022-11-14 16:42:46 +0800

    增加了json文件格式说明

--- run_example.py
+++ run_example.py
@@ -3,6 +3,7 @@ import networkx as nx
 import method
 from single_layer_indicator_use_netwerkx import *
 
+# json格式   {"nodes":[1,2,3], "links":[{"source": "1", "target": "0", "value": 1}]}
 
 json_file_path = "where it locate"
 graph = method.net_build(json_file_path)
